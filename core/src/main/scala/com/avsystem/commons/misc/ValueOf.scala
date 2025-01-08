c90bb9d5d56d42246eb5ee6e4f644e4906778d58
Bartłomiej Kozak <b.kozak@avsystem.com>
2025-01-08 11:15:56 +0100

    Add ValueOf implementation and update ValueOfTest to use ValueOf instead of valueOf.

---
+++ core/src/main/scala/com/avsystem/commons/misc/ValueOf.scala
@@ -0,0 +1,27 @@
+package com.avsystem.commons
+package misc
+
+import scala.quoted.{Expr, Quotes, Type}
+
+import scala.annotation.implicitNotFound
+
+object ValueOf:
+  def apply[T](using vof: ValueOf[T]): T = vof.value
+
+  inline given mkValueOf[T]: ValueOf[T] = ${ singleValueForImpl[T] }
+
+def singleValueForImpl[T: Type](using quotes: Quotes): Expr[ValueOf[T]] = {
+  import quotes.reflect.*
+  val tpe = TypeRepr.of[T]
+  val value = Expr.summon[ValueOf[T]] match
+    case Some(expr: Expr[ValueOf[T]]) => expr
+    case None if tpe.isSingleton =>
+      tpe match
+        case t@ThisType(tref) =>
+          val singleton = This(t.typeSymbol).asExprOf[T]
+          '{ scala.ValueOf($singleton) }
+        case _ =>
+          report.errorAndAbort(s"Cannot find a value of type ${tpe.show}")
+    case None => report.errorAndAbort(s"Cannot find a value of type ${tpe.show}")
+  value
+}
--- core/src/test/scala/com/avsystem/commons/misc/ValueOfTest.scala
+++ core/src/test/scala/com/avsystem/commons/misc/ValueOfTest.scala
@@ -2,6 +2,7 @@ package com.avsystem.commons
 package misc
 
 import org.scalatest.funsuite.AnyFunSuite
+import ValueOf.mkValueOf
 
 object Obj {
   val x: String = "fuu"
@@ -9,29 +10,28 @@ object Obj {
   class Inner {
     val x: String = "fuu"
 
-    def valueOfX: x.type = valueOf[x.type]
+    def valueOfX: x.type = ValueOf[x.type]
 
-//    def valueOfThis: this.type = valueOf[this.type]
+    def valueOfThis: this.type = ValueOf[this.type]
   }
 }
 
-class ValueOfTest extends AnyFunSuite {
+final class ValueOfTest extends AnyFunSuite:
   test("object") {
-    assert(valueOf[Obj.type] == Obj)
+    assert(ValueOf[Obj.type] == Obj)
   }
 
   test("static val") {
-    assert(valueOf[Obj.x.type] == Obj.x)
+    assert(ValueOf[Obj.x.type] == Obj.x)
   }
 
   test("inner val of local") {
     val i = new Obj.Inner
-    assert(valueOf[i.x.type] == i.x)
+    assert(ValueOf[i.x.type] == i.x)
     assert(i.valueOfX == i.x)
   }
 
   test("this") {
     val i = new Obj.Inner
-    //    assert(i.valueOfThis == i)
+    assert(i.valueOfThis == i)
   }
-}
