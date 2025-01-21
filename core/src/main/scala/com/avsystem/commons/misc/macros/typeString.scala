1b0a1a6b2e1a2397685672974644f5eacc7cdf4f
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-01-21 23:59:14 +0100

    typeString macro Impl

--- core/src/main/scala/com/avsystem/commons/misc/macros/typeString.scala
+++ core/src/main/scala/com/avsystem/commons/misc/macros/typeString.scala
@@ -2,30 +2,76 @@ package com.avsystem.commons
 package misc
 package macros
 
-import misc.TypeString
-
+import scala.annotation.tailrec
 import scala.quoted.{Expr, Quotes, Type}
 
-def typeString[T: Type](using quotes: Quotes): Expr[TypeString[T]] = {
+def typeStringImpl[T: Type](using quotes: Quotes): Expr[TypeString[T]] = {
   import quotes.reflect.*
 
-  def mkName: TypeRepr => String = {
-      case MethodType(paramNames, paramInfos, resultType) =>
-        println(s"paramNames: $paramNames")
-        println(s"paramInfos: $paramInfos")
-        println(s"resultType: $resultType")
-        s"${paramInfos.map(mkName)} => ${resultType.show}"
-        ???
-      case t => t.show
-  }
+  def mkName: TypeRepr => String =
+    case ConstantType(value) =>
+      value.toString
+    case ThisType(t) =>
+      s"${t.typeSymbol.name}.type"
+    //    case tpe if tpe.isSingleton =>
+    //      s"${tpe.show(using Printer.TypeReprShortCode)}.type"
+    case tpe: TermRef =>
+      def generatePath: TypeRepr => List[String] =
+        case TermRef(qual, name) => generatePath(qual) ::: (name :: Nil)
+        case _ => Nil
+
+      s"${generatePath(tpe).mkString(".")}.type"
+    case Refinement(parent, name, TypeBounds(low, hi)) if low =:= TypeRepr.of[Nothing] =>
+      s"${mkName(parent)} {type $name <: ${mkName(hi)}}"
+    case Refinement(parent, name, info) =>
+      s"${mkName(parent)} {type $name: ${mkName(info)}}"
+    case tpe@AppliedType(t, args) if tpe.isFunctionType =>
+      val preparedArgs = args.init match
+        case ByNameType(head) :: Nil => s"(=> ${mkName(head)})"
+        case (head: AppliedType) :: Nil => s"(${mkName(head)})"
+        case head :: Nil => mkName(head)
+        case args => args.map(mkName).mkString("(", ", ", ")")
+
+      s"$preparedArgs => ${mkName(args.last)}"
+    case tpe@AppliedType(t, args) if tpe.isTupleN =>
+      s"(${args.map(mkName).mkString(", ")})"
+    case AppliedType(t, args) =>
+      s"${mkName(t)}[${args.map(mkName).mkString(", ")}]"
+    case AnnotatedType(t, annot) =>
+      ???
+    case AndType(t, u) =>
+      s"${mkName(t)} & ${mkName(u)}"
+    case OrType(t, u) =>
+      s"${mkName(t)} | ${mkName(u)}"
+    case MatchType(bound, scrutinee, cases) =>
+      s"${mkName(scrutinee)} match {${cases.map(mkName).mkString(" | ")}}"
+    case ByNameType(tpe) =>
+      s"=> ${mkName(tpe)}"
+    case ParamRef(binder, idx) =>
+      s"${binder.typeSymbol.name}[${idx}]"
+    case RecursiveThis(_) =>
+      ???
+    case MethodType(paramNames, paramTypes, resType) =>
+      s"(${paramNames.zip(paramTypes).map((n, t) => s"${n}: ${mkName(t)}").mkString(", ")}) => ${mkName(resType)}"
+    case PolyType(paramNames, paramBounds, resType) =>
+      s"[${paramNames.zip(paramBounds).map((n, t) => s"${n}: ${mkName(t)}").mkString(", ")}] => ${mkName(resType)}"
+    case TypeLambda(paramNames, paramBounds, resType) =>
+      s"[${paramNames.zip(paramBounds).map((n, t) => s"${n}: ${mkName(t)}").mkString(", ")}] => ${mkName(resType)}"
+    case MatchCase(caseDef, body) =>
+      ???
+    case TypeBounds(low, hi) if low =:= TypeRepr.of[Nothing] =>
+      val left = if low =:= TypeRepr.of[Nothing] then "?" else s"${mkName(low)}"
+      val right = if hi =:= TypeRepr.of[Any] then "" else s" <: ${mkName(hi)}"
+      s"$left$right"
+    case TypeBounds(low, hi) =>
+      s"${mkName(low)} <: ${mkName(hi)}"
+    case NoPrefix() =>
+      ???
+    case t =>
+      printTypeReprInfo(t)
+      //      printSymbolInfo(t.typeSymbol)
+      t.typeSymbol.name
 
   val tpe = Expr(mkName(TypeRepr.of[T]))
   '{ new TypeString(${ tpe }) }
-  //    try typeStringParts(tpe) match {
-  //      case List(Select(pre, TermName("value"))) => pre
-  //      case trees => q"new $MiscPkg.TypeString[$tpe](${mkStringConcat(trees)})"
-  //    } catch {
-  //      case NonConcreteTypeException(stpe) =>
-  //        abort(s"Could not materialize TypeString for $tpe because instance for $stpe is lacking")
-  //    }
 }
--- core/src/test/scala/com/avsystem/commons/misc/TypeStringTest.scala
+++ core/src/test/scala/com/avsystem/commons/misc/TypeStringTest.scala
@@ -1,8 +1,6 @@
 package com.avsystem.commons
 package misc
 
-import misc.TypeString
-
 import org.scalactic.source.Position
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -28,21 +26,21 @@ object TypeStringTest {
     import suite.testTypeString
     testTypeString[Int]("Int")
     testTypeString[LocalAlias]("Int")
-    testTypeString[StaticAlias]("TypeStringTest.StaticAlias")
+    testTypeString[StaticAlias]("Int")
     testTypeString[Integer]("Integer")
     testTypeString[String => Int]("String => Int")
     testTypeString[(=> String) => Int]("(=> String) => Int")
-    testTypeString[T => Int](s"(${TypeString.of[T]}) => Int")
+    testTypeString[T => Int](s"T => Int")
     testTypeString[String => Int => Double]("String => Int => Double")
     testTypeString[(String => Int) => Double]("(String => Int) => Double")
     testTypeString[() => Int]("() => Int")
     testTypeString[(String, Int)]("(String, Int)")
-    testTypeString[A]("TypeStringTest.A")
+    testTypeString[A]("A")
     testTypeString[T](TypeString.of[T])
-    testTypeString[List[T]](s"List[${TypeString.of[T]}]")
-    testTypeString[TypeStringTest#B]("TypeStringTest#B")
-    testTypeString[x.type]("TypeStringTest.x.type")
-    testTypeString[fuu.bar.q.type]("TypeStringTest.fuu.bar.q.type")
+    testTypeString[List[T]](s"List[T]")
+    testTypeString[TypeStringTest#B]("B")
+    testTypeString[x.type]("x.type")
+    testTypeString[fuu.bar.q.type]("fuu.bar.q.type")
     testTypeString[None.type]("None.type")
     testTypeString[this.type]("TypeStringTest.type")
     testTypeString[Set.type]("Set.type")
@@ -54,8 +52,9 @@ object TypeStringTest {
     testTypeString[Serializable {type A <: String}]("Serializable {type A <: String}")
     testTypeString[ {type A = String}]("{type A = String}")
     testTypeString[ {type A}]("{type A}")
-    testTypeString[({type A})#A]("({type A})#A")
-    testTypeString[({type *})# *]("({type *})# *")
+    // Scala 3 doesn't support type projections
+    //    testTypeString[({type A})#A]("({type A})#A")
+    //    testTypeString[({type *})# *]("({type *})# *")
     testTypeString[ {type A >: Null <: String}]("{type A >: Null <: String}")
     testTypeString[ {type A[+X] = Int}]("{type A[+X] = Int}")
     testTypeString[ {type A[X] = List[X]}]("{type A[X] = List[X]}")
@@ -66,12 +65,14 @@ object TypeStringTest {
     testTypeString[ {var lol: Int}]("{var lol: Int}")
     testTypeString[ {def lol: Int}]("{def lol: Int}")
     testTypeString[ {def lol(): Int}]("{def lol(): Int}")
+    // Scala 3 doesn't support polymorphic refinements
     //    testTypeString[ {def lol[A]: A}]("{def lol[A]: A}")
     //    testTypeString[ {def lol[A](x: Int): A}]("{def lol[A](x: Int): A}")
     testTypeString[ {def lol(x: Int): String}]("{def lol(x: Int): String}")
     testTypeString[ {def lol(x: => Int): String}]("{def lol(x: => Int): String}")
     testTypeString[ {def lol(xs: Int*): String}]("{def lol(xs: Int*): String}")
     testTypeString[ {def lol(xs: Int => String*): String}]("{def lol(xs: Int => String*): String}")
+    // Scala 3 doesn't support polymorphic refinements
     //    testTypeString[ {def lol[**](xs: ** *): String}]("{def lol[**](xs: ** *): String}")
     testTypeString[ {def lol(using x: Int, y: String): String}](
       "{def lol(using x: Int, y: String): String}")
@@ -84,7 +85,7 @@ final class TypeStringTest extends AnyFunSuite:
 
   inline def testTypeString[T: TypeString](expected: String)(using pos: Position): Unit =
     test(s"${pos.lineNumber}:$expected") {
-      assert(TypeString.of[T].replace("com.avsystem.commons.macros.", "") == expected)
+      assert(TypeString.of[T].replace("com.avsystem.commons.misc.", "") == expected)
     }
 
   TypeStringTest.defineTests[Double](this)
@@ -105,18 +106,18 @@ final class TypeStringTest extends AnyFunSuite:
 
   testTypeString[Int]("Int")
   testTypeString[Integer]("Integer")
-  testTypeString[TypeStringTest#B]("TypeStringTest#B")
-  testTypeString[A]("TypeStringTest.A")
-  //  testTypeString[B]("B")
+  testTypeString[TypeStringTest#B]("B")
+  testTypeString[A]("A")
+  testTypeString[B]("B")
   testTypeString[x.type]("TypeStringTest.x.type")
-  //  testTypeString[y.type]("y.type")
-  //  testTypeString[fuu.bar.q.type]("fuu.bar.q.type")
+  testTypeString[y.type]("y.type")
+  testTypeString[fuu.bar.q.type]("fuu.bar.q.type")
   testTypeString[None.type]("None.type")
-  //  testTypeString[this.type]("this.type")
+  testTypeString[this.type]("TypeStringTest.this.type")
   testTypeString[List[Int]]("List[Int]")
   testTypeString[Set[?]]("Set[?]")
   testTypeString[Set[? <: String]]("Set[? <: String]")
-  //  testTypeString[AnyRef with Serializable]("AnyRef with Serializable")
+  testTypeString[AnyRef & Serializable]("AnyRef & Serializable")
 
   UnrelatedTypeString.defineTests[String](this)
 
@@ -127,17 +128,17 @@ object UnrelatedTypeString {
   def defineTests[T: TypeString](suite: TypeStringTest): Unit = {
     import suite.testTypeString
     testTypeString[Int]("Int")
-    testTypeString[StaticAlias]("TypeStringTest.StaticAlias")
+    testTypeString[StaticAlias]("Int")
     testTypeString[Integer]("Integer")
     testTypeString[String => Int]("String => Int")
     testTypeString[String => Int => Double]("String => Int => Double")
     testTypeString[(String => Int) => Double]("(String => Int) => Double")
     testTypeString[() => Int]("() => Int")
     testTypeString[(String, Int)]("(String, Int)")
-    testTypeString[A]("TypeStringTest.A")
+    testTypeString[A]("A")
     testTypeString[T](TypeString.of[T])
-    testTypeString[List[T]](s"List[${TypeString.of[T]}]")
-    testTypeString[TypeStringTest#B]("TypeStringTest#B")
+    testTypeString[List[T]](s"List[T]")
+    testTypeString[TypeStringTest#B]("B")
     testTypeString[x.type]("TypeStringTest.x.type")
     testTypeString[fuu.bar.q.type]("TypeStringTest.fuu.bar.q.type")
     testTypeString[None.type]("None.type")
