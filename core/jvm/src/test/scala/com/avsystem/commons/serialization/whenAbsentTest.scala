6cdf4332d3bab843ef80201040de8a9f7977a798
Bartłomiej Kozak <b.kozak@avsystem.com>
2025-02-15 17:20:47 +0100

    whenAbsent impl

---
+++ core/jvm/src/test/scala/com/avsystem/commons/serialization/whenAbsentTest.scala
@@ -0,0 +1,35 @@
+package com.avsystem.commons
+package serialization
+
+import org.scalatest.funsuite.AnyFunSuite
+
+
+final class whenAbsentTest extends AnyFunSuite:
+  @whenAbsent("default")
+  val str: String = whenAbsent.value
+
+  final case class HasDefault(@whenAbsent("default1") str: String = whenAbsent.value)
+
+  final class HasDefault2(@whenAbsent("default2") val str: String = whenAbsent.value)
+
+  @whenAbsent("default3")
+  def method1: String = whenAbsent.value
+
+  def method2(@whenAbsent("default4") param: String = whenAbsent.value): String = param
+
+  test("val") {
+    assert(str == "default")
+  }
+
+  test("case class") {
+    assert(HasDefault().str == "default1")
+  }
+
+  test("class with val") {
+    assert(HasDefault2().str == "default2")
+  }
+
+  test("method") {
+    assert(method1 == "default3")
+    assert(method2() == "default4")
+  }
---
+++ core/src/main/scala/com/avsystem/commons/serialization/macros/whenAbsentValueImpl.scala
@@ -0,0 +1,38 @@
+package com.avsystem.commons
+package serialization.macros
+
+import serialization.whenAbsent
+
+import scala.util.matching.Regex
+
+
+object DefaultValueMethod:
+  private val DefaultValueMethodName: Regex = """(.*)\$default\$(\d+)$""".r
+  private val ConstructorMethodName = "$lessinit$greater"
+
+  def unapply(using quotes: Quotes)(symbol: quotes.reflect.Symbol): Option[quotes.reflect.Symbol] = {
+    import quotes.reflect.*
+    symbol match
+      case ms if ms.isDefDef /* && ms.flags.is(Flags.Synthetic)*/ => ms.name match
+        case DefaultValueMethodName(name, idx) =>
+          val paramIndex = idx.toInt - 1
+          val ownerMethod = name match
+            case `ConstructorMethodName` => ms.owner.companionClass.primaryConstructor
+            case _ => ms.owner.singleMethodMember(name)
+
+          Some(ownerMethod.paramSymss.flatten.apply(paramIndex))
+        case _ => None
+      case _ => None
+  }
+
+def whenAbsentValueImpl[T: Type](using quotes: Quotes): Expr[T] = {
+  import quotes.reflect.*
+  val param = Symbol.spliceOwner.owner match
+    case DefaultValueMethod(p) => p
+    case p => p
+
+  param.getAnnotation(TypeRepr.of[whenAbsent].typeSymbol).map {
+    case Apply(_, List(arg)) => arg.asExprOf[T]
+    case t => report.errorAndAbort(s"unexpected tree for @whenAbsent annotation: ${t.show}")
+  } getOrElse report.errorAndAbort(s"no @whenAbsent annotation found on $param of ${param.owner}")
+}
